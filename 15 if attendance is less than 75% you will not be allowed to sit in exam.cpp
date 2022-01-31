07f0fca3b39511d20176dba9900a616f23d758dc
Shaswat SInha <97851942+Lone0023@users.noreply.github.com>
2022-01-31 17:27:11 +0530

    Add files via upload

--- 15 if attendance is less than 75% you will not be allowed to sit in exam.cpp
+++ 15 if attendance is less than 75% you will not be allowed to sit in exam.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 int main()
 {
-    int days,p,a,attendance;
+    int days,p,a;
+    float attendance;
     cout<<"Enter the total number of working days: ";
     cin>>days;
-    days = days*0.75;
+    int orgn_days = days;
     cout<<"Enter the number of days you were present: ";
     cin>>p;
-        if(p>days){
-            cout<<"You are allowed to sit in the examination";
+    a = orgn_days-p;
+    attendance = orgn_days - a;
+    attendance = (attendance/orgn_days)*100;
+    cout<<"Your attendance is "<<attendance<<"%."<<endl;
+        if(attendance>75){
+            cout<<"You are allowed to sit in the examination.";
         }
         else{
-            cout<<"You are not allowed to sit in the examination";
+            cout<<"You are not allowed to sit in the examination.";
         }
     return 0;
 }
