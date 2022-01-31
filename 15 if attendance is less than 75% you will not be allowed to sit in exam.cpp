76e829cf2af7edb0f4986a4338870b3df3317f44
Shaswat SInha <97851942+Lone0023@users.noreply.github.com>
2022-01-31 18:05:08 +0530

    Add files via upload

--- 15 if attendance is less than 75% you will not be allowed to sit in exam.cpp
+++ 15 if attendance is less than 75% you will not be allowed to sit in exam.cpp
@@ -3,21 +3,33 @@ using namespace std;
 int main()
 {
     int days,p,a;
+    string ans;
+    bool flag=0;
     float attendance;
     cout<<"Enter the total number of working days: ";
     cin>>days;
     int orgn_days = days;
     cout<<"Enter the number of days you were present: ";
     cin>>p;
-    a = orgn_days-p;
+    a = orgn_days-p; // a denotes absent and p denotes present.
     attendance = orgn_days - a;
     attendance = (attendance/orgn_days)*100;
     cout<<"Your attendance is "<<attendance<<"%."<<endl;
         if(attendance>75){
-            cout<<"You are allowed to sit in the examination.";
+            cout<<"You are allowed to sit in the examination."<<endl;;
         }
         else{
-            cout<<"You are not allowed to sit in the examination.";
+                cout<<"Do you have medical cause: ";
+                cin>>ans;
+                    if(ans=="Yes" || ans=="yes" || ans=="YES"){
+                        cout<<"Submit your medical record to the respective authority and you can sit the in the examination."<<endl;
+                    }
+                    else if(ans=="No" || ans=="no" || ans=="NO"){
+                        cout<<"You are not allowed to sit in the examination."<<endl;
+                            }
+                    else{
+                        cout<<"invalid argument entered."<<endl;
+                    }
         }
     return 0;
 }
